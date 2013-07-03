b96400a0a4dc9dd5ed9febbc36597d1175baec8f
Andrew Bonventre <andybons@gmail.com>
2013-07-03 14:18:33 -0400

    simplify example

--- examples/test1.go
+++ examples/test1.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"gogif"
-	"image"
 	"image/gif"
+	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 )
 
@@ -19,20 +18,11 @@ func main() {
 
 	g, err := gif.DecodeAll(f)
 	if err != nil {
-		log.Fatalf("gif.DecodeAll: %q\n", err)
+		log.Fatal(err)
 	}
-	log.Printf("num images: %d, delay: %v, loopcount: %d", len(g.Image), g.Delay, g.LoopCount)
-
-	m = g.Image[0]
-
-	http.HandleFunc("/", handleIndex)
-	fmt.Println("Serving result image at http://locahost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-var m image.Image
-
-func handleIndex(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "image/gif")
-	gogif.Encode(w, m, nil)
+	var buf bytes.Buffer
+	if err := gogif.EncodeAll(&buf, g); err != nil {
+		log.Fatal(err)
+	}
+	ioutil.WriteFile("test1.gif", buf.Bytes(), 0660)
 }
